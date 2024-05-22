0cdf7f5621876ed8849c57e04b84d6132961c8ff
Yves Martinez <yves.oliver.m@gmail.com>
2024-05-22 09:12:43 -0400

    aligned nav bar items and inverted sidebar/body colors

--- react-miniproject/src/components/Footer.jsx
+++ react-miniproject/src/components/Footer.jsx
@@ -1,13 +1,16 @@
-
 const Footer = () => {
-return (
+  return (
     <footer>
-        <img id="footer-logo" src="https://cdn-icons-png.flaticon.com/256/25/25231.png" alt="GitHub Logo" />
-        <a href="https://github.com/yvesm001/react-miniproject"> Github Repository</a> 
+      <img
+        id="footer-logo"
+        src="https://cdn-icons-png.flaticon.com/256/25/25231.png"
+        alt="GitHub Logo"
+      />
+      <a href="https://github.com/yvesm001/react-miniproject" target="_blank">
+        {" "}
+        Github Repository
+      </a>
     </footer>
-
-
-
-)
-}
+  );
+};
 export default Footer;
